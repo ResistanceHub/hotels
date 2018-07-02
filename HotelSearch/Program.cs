4eb7312211047473200e4bebcf0311333b154601
Ping Wu <Ping.Wu@insightsoftware.com>
2018-07-02 11:30:00 +0100

    dissmiss popups

--- HotelSearch/Program.cs
+++ HotelSearch/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
@@ -12,15 +9,23 @@ namespace HotelSearch
     {
         static void Main(string[] args)
         {
-
+            //search with the hotel names in London area
             IWebDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://www.trivago.co.uk");
             driver.FindElement(By.Id("horus-querytext")).SendKeys("London");
-            driver.FindElement(By.ClassName("horus-btn-search")).Click();
+            driver.FindElement(By.ClassName("horus-btn-search")).Click();// how do I know need to find element by classname and how to decide the content 'horus-btn-search' as the class said that it is 'btn btn--primary horus-btn-search'?
+            Thread.Sleep(1000);//to delay to allow the popup happen
+            driver.FindElement(By.ClassName("df_overlay_close_wrap")).Click();//dismiss popups
 
-            var ListOfHotels = driver.FindElements(By.CssSelector("h3"));
-            for (int i = 0; i <= 15; i++) {
-            }
+            
+
+
+            //save all searched result to the list
+            //var ListOfHotels = driver.FindElements(By.CssSelector("h3"));
+            //for (int i = 0; i <= 15; i++)
+            //{
+
+            //}
 
             Console.ReadKey();
         }
