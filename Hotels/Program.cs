4142bbc9e4d640983c422a9d2e66563e0de32392
Dilini Prasangika <dilini.prasangika@insightsoftware.com>
2018-07-03 00:13:11 +0100

    Created the console application and added code to search London hotels

---
+++ Hotels/Program.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.trivago.co.uk/");
+            IWebElement city = driver.FindElement(By.Id("horus-querytext"));
+            city.SendKeys("London");
+            driver.FindElement(By.ClassName("horus-btn-search__icon")).Click();
+
+        }
+    }
+}
+
