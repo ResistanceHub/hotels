2fae18d243e76a1ea1a8f79507cef6db14cf4c2c
Dilini Prasangika <dilini.prasangika@insightsoftware.com>
2018-07-03 17:12:44 +0100

    Added code to print Hotel name and rating into the console for hotels listed in the first page

--- Hotels/Program.cs
+++ Hotels/Program.cs
@@ -17,6 +17,19 @@ namespace Hotels
             IWebElement city = driver.FindElement(By.Id("horus-querytext"));
             city.SendKeys("London");
             driver.FindElement(By.ClassName("horus-btn-search__icon")).Click();
+            //driver.FindElement(By.ClassName("df_overlay_close_wrap overlay__close")).Click();
+
+
+            for(int i = 1; i <=25; i++)
+            {
+                System.Threading.Thread.Sleep(TimeSpan.FromSeconds(5));
+                var hotelName = driver.FindElement(By.CssSelector("#js_item_list_container > section > ol > li:nth-Child(" + i + ") div > h3")).Text;
+                var rating = driver.FindElement(By.CssSelector("#js_item_list_container > section > ol > li:nth-Child(" + i + ") div span.rating-box__value")).Text;
+                Console.WriteLine(hotelName + ", " + rating);
+            }
+            Console.ReadKey();
+      
+
 
         }
     }
