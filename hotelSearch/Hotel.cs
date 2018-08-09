a82c5d34e82a695c7c02784a9f541be60a9ffe8c
Ping Wu <Ping.Wu@insightsoftware.com>
2018-08-09 13:00:57 +0100

    Added rating and price info by using hotels class, and write all info to file

---
+++ hotelSearch/Hotel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSearch
+{
+    public class Hotel
+    {
+        // these are private fields
+        private String name;
+        private String rating;
+        private String price;
+
+        // this is a public property
+        public string Name => name;
+        public string Rating => rating;
+
+        public string Price
+        {
+            get { return price; }
+        }
+
+        public Hotel(string name, string rating,string price)
+        {         
+            this.name = name;
+            this.rating = rating;
+            this.price = price;
+        }
+
+        
+    }
+}
