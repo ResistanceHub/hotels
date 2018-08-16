72dd1528cdc8e1e543c720363f34b7504d5379a0
Ping Wu <Ping.Wu@insightsoftware.com>
2018-08-16 10:27:16 +0100

    Seprate Page Object 2 - SearchPage into individual classes

--- hotelSearch/Hotel.cs
+++ hotelSearch/Hotel.cs
@@ -9,24 +9,20 @@ namespace HotelSearch
     public class Hotel
     {
         // these are private fields
-        private String name;
-        private String rating;
-        private String price;
+        private String _name;
+        private String _rating;
+        private String _price;
 
         // this is a public property
-        public string Name => name;
-        public string Rating => rating;
-
-        public string Price
-        {
-            get { return price; }
-        }
+        public string Name => _name;
+        public string Rating => _rating;
+        public string Price => _price;
 
         public Hotel(string name, string rating,string price)
         {         
-            this.name = name;
-            this.rating = rating;
-            this.price = price;
+            _name = name;
+            _rating = rating;
+            _price = price;
         }
 
         
---
+++ hotelSearch/PageObjects/SearchPage.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace HotelSearch.PageObjects
+{
+    class SearchPage
+    {
+        private readonly IWebDriver _driver;
+
+        public SearchPage(IWebDriver driver)
+        {
+            _driver = driver; //make private to public
+        }
+
+        public void DismissPopUps()
+        {
+            //dismiss PopUps       
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            wait.Until(drv => drv.FindElement(By.ClassName("df_overlay_close_wrap"))).Click();
+            Thread.Sleep(TimeSpan.FromSeconds(7)); //wait until result show up
+        }
+        public List<Hotel> GetHotels() //return Type needed
+        {
+            //Add the Hotel's informations to the list
+            var elementsOfHotels =
+                _driver.FindElements(By.ClassName("hotel")); //find parent node to cover all info we need
+            // create hotels using LINQ select
+            var hotels = elementsOfHotels.Select(hotelElement => {
+                var name = hotelElement.FindElement(By.CssSelector(".name__copytext")).Text;
+                var rating = hotelElement.FindElement(By.ClassName("rating-box__value")).Text;
+                var price = hotelElement.FindElement(By.ClassName("item__best-price")).Text;
+                return new Hotel(name, rating, price);
+            }).ToList();
+
+            return hotels;
+        }
+
+        public void SaveHotels(List<Hotel> hotels)
+        {
+            //Save Hotel Name list on screen or into CSV file
+            const string path = @"..\..\SearchResult\hotels.csv"; //put reltive path to Debug folder           
+            for (var i = 0; i <= hotels.Count - 1; i++)
+            {
+                var hotel = hotels[i];
+                Console.WriteLine($"{i + 1},{hotel.Name}, {hotel.Rating},{hotel.Price}");
+            }           
+            var fs = new FileStream(path, FileMode.Create);
+            using (var hotelsResult = new StreamWriter(fs, Encoding.UTF8)
+            ) // Get correct encoding format for price, see https://msdn.microsoft.com/en-us/library/72d9f8d5(v=vs.110).aspx
+            {
+                Console.WriteLine("First Page Hotels");
+                Console.WriteLine($"{"ID"}, {"Name"} , {"Rating"} , {"Price"}");
+
+                hotelsResult.WriteLine("First Page Hotels");
+                hotelsResult.WriteLine($"{"ID"}, {"Name"} , {"Rating"} , {"Price"}");
+
+                for (var i = 0; i < hotels.Count; i++) {
+                    Hotel hotel = hotels[i];
+                    Console.WriteLine($"{i + 1},{hotel.Name}, {hotel.Rating},{hotel.Price}");
+                    hotelsResult.WriteLine($"{i + 1},{hotel.Name},{hotel.Rating},{hotel.Price}");
+                }
+
+            }
+        }
+    }
+}
